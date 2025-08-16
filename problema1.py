898b853f0b9696556cbf130736bb94e042143c2e
enbor <enbor2012@hotmail.com>
2025-08-15 23:12:55 -0600

    Tareas
    Son mis 3 tareas.

---
+++ problema1.py
@@ -0,0 +1,7 @@
+numero=int(input("Ingrese un número entero"))
+if numero<0:
+    print("El número es negativo")
+elif número>0:
+    print("el número es positivo")
+else:
+    print("el numero es cero")
---
+++ problema3.py
@@ -0,0 +1,12 @@
+n = int(input("ingrse el numero de empleados"))
+salarios[]
+for i in range(n)
+    salario = float(input("ingrese el salario de empleado"))
+    salario.append(salario)
+
+promedio = sum(salario)/n
+promedio = sum(salario si salario>promedio)
+desviacion_estandar = statistics.stdev(salarios)
+print("salario promedio: promedio")
+print("numero de empleado con salario por encima del promedio")
+print("desviacion estandar de los salarios")
